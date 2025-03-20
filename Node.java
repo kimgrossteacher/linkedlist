bac7b14fd7c0f14f5a5165531b0a38098420376e
kimgrossteacher <148360456+kimgrossteacher@users.noreply.github.com>
2025-03-20 16:55:13 -0600

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,53 +1,36 @@
 public class Node {
 
     Node next;
-
     Node previous;
+    StudentObject data;
 
-    Object data;
-
-    Node (Object data, Node next,Node previous){
-
+    Node (StudentObject data, Node next,Node previous){
         this.data=data;
-
         this.next=next;
-
         this.previous=previous;
-
         }
 
     public void setNext(Node next){
-
             this.next=next;
-
         }
 
     public void setPrevious (Node previous){
-
             this.previous=previous;
-
         }
 
     public Node getNext(){
-
             return next;
-
         }
 
     public Node getPrevious(){
-
             return previous;
-
         }
 
-    public void setData(Object data){
-
+    public void setData(StudentObject data){
             this.data=data;
-
         }
 
     public Object getData(){
-
         return data;
 
     }    
